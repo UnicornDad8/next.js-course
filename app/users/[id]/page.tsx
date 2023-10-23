81de321c7d88ab0d21125decc3bf254ab08c98b3
UnicornDad8 <unicorndad8@gmail.com>
2023-10-23 12:34:21 -0300

    Handling not found errors

--- app/users/[id]/page.tsx
+++ app/users/[id]/page.tsx
@@ -1,10 +1,13 @@
 import React from "react";
+import { notFound } from "@/node_modules/next/navigation";
 
 interface Props {
   params: { id: number };
 }
 
 const UserDetailPage = ({ params: { id } }: Props) => {
+  if (id > 10) notFound();
+
   return <div>User Detail Page... {id}</div>;
 };
 
