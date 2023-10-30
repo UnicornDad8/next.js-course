987876276ab45660329bc75ea09548ae1519e459
UnicornDad8 <unicorndad8@gmail.com>
2023-10-30 12:15:06 -0300

    Configuring GoogleProvider

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,5 +1,14 @@
 import NextAuth from "next-auth";
+import GoogleProvider from "next-auth/providers/google";
 
-const handler = NextAuth({});
+const handler = NextAuth({
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID!,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      checks: ["none"],
+    }),
+  ],
+});
 
 export { handler as GET, handler as POST };
