7201de92cf2f2fec602ce2ee88f508d480094e0f
UnicornDad8 <unicorndad8@gmail.com>
2023-11-07 10:41:29 -0300

    Optimazing images

--- app/page.tsx
+++ app/page.tsx
@@ -1,25 +1,18 @@
-"use client";
+import Image from "next/image";
+import coffee from "@/public/images/coffee.jpg";
 
 export default function Home() {
   return (
-    <main>
-      <h1>Hello World</h1>
-      <button
-        onClick={async () => {
-          const _ = (await import("lodash")).default;
-
-          const users = [
-            { name: "c" },
-            { name: "b" },
-            { name: "a" },
-          ];
-
-          const sorted = _.orderBy(users, ["name"]);
-          console.log(sorted);
-        }}
-      >
-        Show
-      </button>
+    <main className="relative h-screen">
+      <Image
+        src="https://bit.ly/react-cover"
+        alt="React"
+        fill
+        className="object-cover"
+        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
+        quality={100}
+        priority
+      />
     </main>
   );
 }
