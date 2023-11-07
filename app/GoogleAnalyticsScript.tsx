375f487328ac3dfd651c96af31503ae1716c97c9
UnicornDad8 <unicorndad8@gmail.com>
2023-11-07 10:48:35 -0300

    Using third-party scripts

--- app/GoogleAnalyticsScript.tsx
+++ app/GoogleAnalyticsScript.tsx
@@ -1,19 +1,22 @@
-import Script from 'next/script'
-import React from 'react'
+import Script from "next/script";
+import React from "react";
 
 const GoogleAnalyticsScript = () => {
   return (
     <>
-    <Script async src="https://www.googletagmanager.com/gtag/js?id=G-E720JHXSJ2" />
-    <Script id="google-analytics">
-      {`window.dataLayer = window.dataLayer || [];
+      <Script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-E720JHXSJ2"
+      />
+      <Script id="google-analytics">
+        {`window.dataLayer = window.dataLayer || [];
       function gtag(){dataLayer.push(arguments);}
       gtag('js', new Date());
 
       gtag('config', 'G-E720JHXSJ1');`}
-    </Script>
+      </Script>
     </>
-  )
-}
+  );
+};
 
-export default GoogleAnalyticsScript
+export default GoogleAnalyticsScript;
