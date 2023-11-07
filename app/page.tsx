062b6d2e6e7dfe171d29f52da264e36e5d40d8cd
UnicornDad8 <unicorndad8@gmail.com>
2023-11-07 11:53:41 -0300

    Experimenting lazy loading a component

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
+import dynamic from "next/dynamic";
+const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
+  ssr: false,
+  loading: () => <p>Loading...</p>,
+});
 
 export default function Home() {
+  const [isVisible, setVisible] = useState(false);
+
   return (
     <main>
       <h1>Hello World</h1>
+      <button onClick={() => setVisible(true)}>Show</button>
+      {isVisible && <HeavyComponent />}
     </main>
   );
 }
