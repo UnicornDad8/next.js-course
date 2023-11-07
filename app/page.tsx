2ff51f2a23c43be432ceb639cbf89bebf6f34909
UnicornDad8 <unicorndad8@gmail.com>
2023-11-07 12:03:49 -0300

    Lazy loading with lodash

--- app/page.tsx
+++ app/page.tsx
@@ -1,20 +1,24 @@
 "use client";
 
-import React, { useState } from "react";
-import dynamic from "next/dynamic";
-const HeavyComponent = dynamic(() => import("./components/HeavyComponent"), {
-  ssr: false,
-  loading: () => <p>Loading...</p>,
-});
+import React from "react";
+import _ from "lodash";
 
 export default function Home() {
-  const [isVisible, setVisible] = useState(false);
-
   return (
     <main>
       <h1>Hello World</h1>
-      <button onClick={() => setVisible(true)}>Show</button>
-      {isVisible && <HeavyComponent />}
+      <button
+        onClick={async () => {
+          const _ = (await import("lodash")).default;
+
+          const users = [{ name: "c" }, { name: "b" }, { name: "a" }];
+
+          const sorted = _.orderBy(users, ["name"]);
+          console.log(sorted);
+        }}
+      >
+        Show
+      </button>
     </main>
   );
 }
