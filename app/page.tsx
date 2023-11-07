be96c408a48eb6a77254b698b3391ca5dec61714
UnicornDad8 <unicorndad8@gmail.com>
2023-11-07 11:03:00 -0300

    Using custom fonts

--- app/page.tsx
+++ app/page.tsx
@@ -1,18 +1,9 @@
-import Image from "next/image";
-import coffee from "@/public/images/coffee.jpg";
+import React from "react";
 
 export default function Home() {
   return (
-    <main className="relative h-screen">
-      <Image
-        src="https://bit.ly/react-cover"
-        alt="React"
-        fill
-        className="object-cover"
-        sizes="(max-width: 480px) 100vw, (max-width: 768px) 50vw, 33vw"
-        quality={100}
-        priority
-      />
+    <main>
+      <h1>Hello World</h1>
     </main>
   );
 }
